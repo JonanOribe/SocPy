58e883f457f5734e3828e57cea3a8f5df1331065
Jon Ander Oribe <jonan.oribe@gmail.com>
2022-04-20 20:34:25 +0200

    Basics

--- socpy/social/urls.py
+++ socpy/social/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView,PostDetailsView
 
 urlpatterns = [
     path('',PostListView.as_view(),name='post-list'),
+    path('post/<int:pk>',PostDetailsView.as_view(),name='post-detail')
 ]
