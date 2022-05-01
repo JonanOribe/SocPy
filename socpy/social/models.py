f49ae2b802406e3baa51d80b3990bd3ae4b2a503
Jon Ander Oribe <jonan.oribe@gmail.com>
2022-05-01 13:46:31 +0200

    Styles

--- socpy/social/models.py
+++ socpy/social/models.py
@@ -36,12 +36,15 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, primary_key=True, verbose_name='user',
                                 related_name='profile', on_delete=models.CASCADE)
     name = models.CharField(max_length=30, blank=True, null=True)
+    role = models.CharField(max_length=60, blank=True, null=True)
     bio = models.TextField(max_length=500, blank=True, null=True)
     birth_date = models.DateField(null=True, blank=True)
     location = models.CharField(max_length=100, blank=True, null=True)
     picture = models.ImageField(
         upload_to=UPLOADS_PATH, default=UPLOADS_PATH+'/default.png', blank=True)
     followers = models.ManyToManyField(User, blank=True,related_name='followers')
+    points = models.SmallIntegerField(null=True)
+    ranking = models.PositiveSmallIntegerField(null=True)
 
 @receiver(post_save,sender=User)
 def create_user_profile(sender,instance,created,**kwargs):
